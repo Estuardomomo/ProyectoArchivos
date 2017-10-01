8433f286701557848a278a236232def152612137
Kevin Oswaldo Ubedo Zamora <kev.ubedo@gmail.com>
2017-10-01 11:49:54 -0600

    Revert "Merge branch 'master' into Mantenimiento"
    This reverts commit e134942d5cc05eb7ec6607fd8a001745fe64b899.

--- ProyectoArchivos/src/Ventanas/Backup.java
+++ ProyectoArchivos/src/Ventanas/Backup.java
@@ -4,8 +4,6 @@
  * and open the template in the editor.
  */
 package Ventanas;
-
-import static java.awt.image.ImageObserver.WIDTH;
 import java.io.File;
 import javax.swing.JFileChooser;
 import java.io.FileNotFoundException;
@@ -17,10 +15,6 @@ import java.nio.file.StandardCopyOption;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.Date;
-import javax.swing.JOptionPane;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 /**
  *
  * @author jsala
@@ -38,11 +32,6 @@ public class Backup extends javax.swing.JFrame {
     public Backup() {
         initComponents();
     }
-    
-    public void setUser(User user){
-        this.user = user;
-        user.setRol("1");
-    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -58,7 +47,6 @@ public class Backup extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -79,11 +67,6 @@ public class Backup extends javax.swing.JFrame {
         jLabel1.setText("Copia de seguridad");
 
         jButton2.setText("Ver bitácora");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
 
         jButton3.setText("Realizar Backup");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
@@ -92,31 +75,12 @@ public class Backup extends javax.swing.JFrame {
             }
         });
 
-        jButton4.setText("Salir");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(89, 89, 89)
-                        .addComponent(jLabel1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(158, 158, 158)
-                        .addComponent(jButton3)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(jButton4))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jButton2))
@@ -126,6 +90,15 @@ public class Backup extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
                         .addComponent(jButton1)))
                 .addGap(37, 37, 37))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(89, 89, 89)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(158, 158, 158)
+                        .addComponent(jButton3)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -138,35 +111,18 @@ public class Backup extends javax.swing.JFrame {
                     .addComponent(TF_backupPath, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(36, 36, 36)
                 .addComponent(jButton3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 14, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
                 .addComponent(jButton2)
-                .addGap(18, 18, 18)
-                .addComponent(jButton4)
-                .addGap(30, 30, 30))
+                .addGap(44, 44, 44))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void ValidateUserRol(){
-        if (!(user.getRol() == "1")) {
-            jButton1.setEnabled(false);
-            jButton2.setEnabled(false);
-            jButton3.setEnabled(false);
-            JOptionPane.showMessageDialog(null, "¡Lo sentimos! No puede realizar esta acción porque no es administrador", "Error de administrador", WIDTH);
-        }
-        else{
-            jButton1.setEnabled(true);
-            jButton2.setEnabled(true);
-            jButton3.setEnabled(true);
-        }
-    }
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        TF_backupPath.setEnabled(false);
         JFileChooser dialog = new JFileChooser();
-        //FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivo de texto", "");
-        dialog.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        //FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivo de texto", "txt");
         File newFile;
         String filePath;
         // dialog.setFileFilter(filter);
@@ -181,16 +137,15 @@ public class Backup extends javax.swing.JFrame {
 
     private void TF_backupPathActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TF_backupPathActionPerformed
         // TODO add your handling code here:
-        
+        TF_backupPath.setEnabled(false);
     }//GEN-LAST:event_TF_backupPathActionPerformed
 
-    //Metodo para copiar todos los archivos a una nueva ruta
     private void copyFile(String origin, String actual){
         //static final Logger LOGGER = Logger.getAnonymousLogger();
         try{
             Path originPath = Paths.get(origin);
             Path actualPath = Paths.get(actual);
-            Files.copy(originPath, actualPath, StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(originPath, actualPath, StandardCopyOption.REPLACE_EXISTING );
         }catch(FileNotFoundException ex){
             LOGGER.log(Level.SEVERE, ex.getMessage());
         }catch(IOException ex){
@@ -198,43 +153,22 @@ public class Backup extends javax.swing.JFrame {
         }      
             
     }
-    //Método para escribir en bitácora
     private void writeBitacora(){
         f.createFile(bitacoraPath);
         Date date = new Date();
-        DateFormat hourAndDate = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
-        f.writeOnBitacora(bitacoraPath, TF_backupPath.getText(), user.getUser(), hourAndDate.format(date));
+        long lnMiliseconds  = date.getTime(); //fecha actual
+        
+        java.sql.Date sqlDate = new java.sql.Date(lnMiliseconds);
+        f.writeOnBitacora(bitacoraPath, TF_backupPath.getText(), user.getUser(), sqlDate.toString());
         
     }
-    //Botón para realizar la copia de seguridad
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-        ValidateUserRol();
         f.createFolder(TF_backupPath.getText());
         copyFile(CPath ,TF_backupPath.getText());
-        File origin = new File(CPath);
-        String[] paths = origin.list();
-        for (int i = 0; i < paths.length; i++) {
-            copyFile(CPath + paths[i] ,TF_backupPath.getText()+"\\" + paths[i]);
-        }
-        JOptionPane.showMessageDialog(null,"Se realizó la copia de seguridad con éxito", "Respaldo de información", WIDTH);
         writeBitacora();
     }//GEN-LAST:event_jButton3ActionPerformed
 
-    //Botón salir
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
-        this.setVisible(false);
-    }//GEN-LAST:event_jButton4ActionPerformed
-
-    //Botón para ver la bitácora
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-        f.AbrirArchivo(bitacoraPath);
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    //Abrir el archivo "bitácora_backup.txt"
-   
     /**
      * @param args the command line arguments
      */
@@ -275,7 +209,6 @@ public class Backup extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
