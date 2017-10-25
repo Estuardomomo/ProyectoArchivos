164d9b2fd6383f510c9b0712eccf75d902e9a26b
SC_Jackie <jackiesalazar20@gmail.com>
2017-10-25 03:30:16 -0600

    Estoy creando todo de nuevo, este commit solo es para copiar la clase SecuencialIndizado ya que no se encuentra en esta rama.

--- ProyectoArchivos/src/Ventanas/MantenimientoGrupos.java
+++ ProyectoArchivos/src/Ventanas/MantenimientoGrupos.java
@@ -15,6 +15,7 @@ import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
+import javax.swing.DefaultListModel;
 import javax.swing.JOptionPane;
 
 /**
@@ -43,6 +44,7 @@ public class MantenimientoGrupos extends javax.swing.JFrame {
     Boolean GNombre;
     Boolean Gintegrantes;
     Boolean GDescripcion;
+    DefaultListModel listaGrupos = new DefaultListModel();
     //Constructores
     public MantenimientoGrupos() {
         initComponents();
@@ -82,6 +84,7 @@ public class MantenimientoGrupos extends javax.swing.JFrame {
         tfGNDescripcion = new javax.swing.JTextArea();
         btnGModificar = new javax.swing.JButton();
         btnVolver = new javax.swing.JButton();
+        AgregarAmigos = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -114,6 +117,12 @@ public class MantenimientoGrupos extends javax.swing.JFrame {
 
         jLabel5.setText("Modificar un grupo");
 
+        cbGrupos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbGruposActionPerformed(evt);
+            }
+        });
+
         jLabel6.setText("Núm miembros:");
 
         tfGNDescripcion.setColumns(20);
@@ -134,21 +143,19 @@ public class MantenimientoGrupos extends javax.swing.JFrame {
             }
         });
 
+        AgregarAmigos.setText("Añadir integrantes");
+        AgregarAmigos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AgregarAmigosActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(61, 61, 61)
-                        .addComponent(jLabel4)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(cbGrupos2, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(btnGEliminar)
-                        .addGap(18, 18, 18)
-                        .addComponent(btnVolver))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -188,7 +195,19 @@ public class MantenimientoGrupos extends javax.swing.JFrame {
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(btnGModificar)
-                                .addGap(39, 39, 39)))))
+                                .addGap(39, 39, 39))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(61, 61, 61)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(AgregarAmigos)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(cbGrupos2, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(btnGEliminar)))
+                        .addGap(18, 18, 18)
+                        .addComponent(btnVolver)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -224,7 +243,9 @@ public class MantenimientoGrupos extends javax.swing.JFrame {
                             .addComponent(jLabel4)
                             .addComponent(cbGrupos2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(btnGEliminar))
-                        .addContainerGap(30, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(AgregarAmigos)
+                        .addGap(0, 43, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(btnVolver)
@@ -319,6 +340,17 @@ public class MantenimientoGrupos extends javax.swing.JFrame {
         this.setVisible(false);
     }//GEN-LAST:event_btnVolverActionPerformed
 
+    private void AgregarAmigosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AgregarAmigosActionPerformed
+        // TODO add your handling code here:
+        GrupoAmigos a = new GrupoAmigos(listaGrupos,Usuario);
+        a.setVisible(true);
+                
+    }//GEN-LAST:event_AgregarAmigosActionPerformed
+
+    private void cbGruposActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbGruposActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cbGruposActionPerformed
+
     private void ActualizarOpciones()
     {
         cbGrupos.removeAllItems();
@@ -330,6 +362,7 @@ public class MantenimientoGrupos extends javax.swing.JFrame {
                 String[] Grupos = Datos[i].split(Pattern.quote(","));
                 cbGrupos.addItem(Grupos[1]);
                 cbGrupos2.addItem(Grupos[1]);
+                listaGrupos.add(i,Grupos[1]);
             }
         }
     }
@@ -369,6 +402,7 @@ public class MantenimientoGrupos extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton AgregarAmigos;
     private javax.swing.JButton btnGCrear;
     private javax.swing.JButton btnGEliminar;
     private javax.swing.JButton btnGModificar;
